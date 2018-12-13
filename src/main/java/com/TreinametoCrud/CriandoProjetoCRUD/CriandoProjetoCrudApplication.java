bc2f61a64bc01d1aef6153ce3b929b14671c5a52
HigorLucas <higor.freitas@neppoadm.com.br>
2018-12-13 10:44:57 -0200

    Init crud

---
+++ src/main/java/com/TreinametoCrud/CriandoProjetoCRUD/CriandoProjetoCrudApplication.java
@@ -0,0 +1,14 @@
+package com.TreinametoCrud.CriandoProjetoCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriandoProjetoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriandoProjetoCrudApplication.class, args);
+	}
+
+}
+
